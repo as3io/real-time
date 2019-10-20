310ee21fac732c58a067d5de91c5006a1ab25f94
Jacob Bare <jacob@limit0.io>
2019-10-19 21:16:00 -0500

    Create mongoose connection and start/stop/ping

---
+++ services/graphql/src/mongoose/connection.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const env = require('../env');
+
+const { MONGO_DSN } = env;
+
+module.exports = mongoose.createConnection(MONGO_DSN, {
+  // autoIndex: env.NODE_ENV !== 'production',
+  bufferMaxEntries: 0, // Default -1
+  ignoreUndefined: true,
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
--- services/graphql/src/services.js
+++ services/graphql/src/services.js
@@ -1,5 +1,23 @@
+const log = require('fancy-log');
+const { green, gray } = require('chalk');
+const mongoose = require('./mongoose/connection');
+const pkg = require('../package.json');
+
 module.exports = {
-  start: async () => 'Started!',
-  ping: async () => 'Pong!',
-  stop: async () => 'Stoppped!',
+  start: async () => {
+    const m = await mongoose;
+    log(`Database ${green('connected')} on ${gray(m.client.s.url)}`);
+  },
+  ping: async () => {
+    const args = [{ _id: pkg.name }, { $set: { last: new Date() } }, { upsert: true }];
+    await Promise.all([
+      mongoose.db.command({ ping: 1 }),
+      mongoose.db.collection('pings').updateOne(...args),
+    ]);
+    return 'MongoDB ping successfully.';
+  },
+  stop: async () => {
+    await mongoose.close();
+    log('Database disconnected');
+  },
 };
