e2accc4ecda86fb1304619c3d6965a382ea1c714
Jacob Bare <jacob@limit0.io>
2019-10-19 22:10:26 -0500

    Add note about potential watch race condition

--- services/graphql/src/mongoose/models/story.js
+++ services/graphql/src/mongoose/models/story.js
@@ -5,6 +5,7 @@ const schema = require('../schema/story');
 const Story = connection.model('story', schema, 'stories');
 
 Story.watch().on('change', (data) => {
+  // @todo Is it possible there could be race-like conditions with many updates?
   log('Story change', data);
 });
 
