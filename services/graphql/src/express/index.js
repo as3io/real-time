9d79e7e366d3a4dc8b0aef4185c32f09131c931b
Jacob Bare <jacob@limit0.io>
2019-10-19 21:34:08 -0500

    Create gql schema and apply to server

--- services/graphql/src/express/index.js
+++ services/graphql/src/express/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const schema = require('../schema');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.json({});
+const server = new ApolloServer({
+  schema,
+  ontext: async ({ req }) => {
+    // Simulate a logged-in user.
+    const userEmail = req.get('x-user-email');
+    return { userEmail };
+  },
 });
+
+server.applyMiddleware({ app, path: '/' });
 
 module.exports = app;
