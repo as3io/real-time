bb1e48786ac8bd8516bd530561a60fcef8d8a3ca
Jacob Bare <jacob@limit0.io>
2019-10-21 08:59:01 -0500

    Apply apollo provider to app

--- services/manage/src/main.js
+++ services/manage/src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue';
+import VueApollo from 'vue-apollo';
 import App from './App.vue';
 import router from './router';
+import createProvider from './apollo/create-provider';
 
+Vue.use(VueApollo);
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  apolloProvider: createProvider({ uri: 'http://localhost:17420' }),
   render: h => h(App),
 }).$mount('#app');
